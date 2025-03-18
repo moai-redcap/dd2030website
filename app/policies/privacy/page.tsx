795495add2a0855820644aceaf0319df7b8c9967
なのくろ <nanocloudx@gmail.com>
2025-03-18 14:48:47 +0900

    add policies

---
+++ app/policies/privacy/page.tsx
@@ -0,0 +1,18 @@
+import '../policies.css'
+import {Box, Heading} from '@chakra-ui/react'
+
+export default function Page() {
+  return (
+    <Box
+      maxW={'800px'}
+      mx={'auto'}
+      color={'#333'}
+      mt={10}
+      mb={20}
+      fontSize={'sm'}
+    >
+      <Heading fontSize={'2xl'} mb={10} textAlign={'center'}>プライバシーポリシー</Heading>
+      <p>準備中です</p>
+    </Box>
+  )
+}
