5c852a81b1667170b3a96746301300ec0cc641d5
sasano <masatosasano2@gmail.com>
2025-05-16 20:03:50 +0900

    選挙活動ではなく政治活動であることを踏まえて文章を修正

--- app/case/idobata/page.tsx
+++ app/case/idobata/page.tsx
@@ -4,9 +4,9 @@ export default async function Page() {
     <div>
       <section className="mx-auto max-w-xl mt-10">
         <h2 className="text-3xl">いどばたの活用事例</h2>
-        <h3 className="text-2xl mt-8">2030年参院選×チームみらい</h3>
+        <h3 className="text-2xl mt-8">チームみらいによる政策提案のプラッシュアップ</h3>
         <p className="mt-4">
-          2030年の参議院選挙に関する政策集に対する変更提案を広く募集する試みが行われています。
+          政策集に対して変更提案を広く募集するためにいどばた政策立案が使われています。
           詳細は<a href="https://policy.team-mir.ai/view/">こちら</a>
         </p>
       </section>
