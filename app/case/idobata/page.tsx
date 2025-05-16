18c7aaab70c2fe05a5abdcaf3d5d71d9489541c2
sasano <masatosasano2@gmail.com>
2025-05-16 19:38:36 +0900

    いどばたの事例としてチームみらいの例を追加

--- app/case/idobata/page.tsx
+++ app/case/idobata/page.tsx
@@ -4,7 +4,11 @@ export default async function Page() {
     <div>
       <section className="mx-auto max-w-xl mt-10">
         <h2 className="text-3xl">いどばたの活用事例</h2>
-        <p className="mt-4">準備中です</p>
+        <h3 className="text-2xl mt-8">2030年参院選×チームみらい</h3>
+        <p className="mt-4">
+          2030年の参議院選挙に関する政策集に対する変更提案を広く募集する試みが行われています。
+          詳細は<a href="https://policy.team-mir.ai/view/">こちら</a>
+        </p>
       </section>
       <CoCreation />
     </div>
