ccb6f077f7787c8e06724cf5320df41769870fdd
moai-redcap <oscar.gaddress@gmail.com>
2025-05-17 14:42:19 +0900

    logoコンペページの作成1

---
+++ app/logo-compe/page.tsx
@@ -0,0 +1,18 @@
+import path from 'path'
+import fs from 'fs'
+import { marked } from 'marked'
+import { Markdown } from '@/components/Markdown'
+import { CoCreation } from '@/components/CoCreation'
+
+export default async function Page() {
+  const filePath = path.join(process.cwd(), 'markdown', 'logo-compe.md')
+  const markdown = fs.readFileSync(filePath, 'utf-8')
+  const content = await marked(markdown)
+
+  return (
+    <div className="max-w-xl mx-auto">
+      <Markdown content={content} />
+      <CoCreation />
+    </div>
+  )
+}
---
+++ markdown/logo-conpe.md
@@ -0,0 +1,15 @@
+## デジタル民主主義 2030 ロゴデザインコンペ
+
+### 案 1
+
+1[(/images/logos/dd2023_logo_1_1.jpg)]
+1[(/images/logos/dd2023_logo_1_2.jpg)]
+1[(/images/logos/dd2023_logo_1_3.jpg)]
+
+### 案 2
+
+### 案 3
+
+### 案 4
+
+![鈴木 健](/images/board/suzuki.jpg)
